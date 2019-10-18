b20e4a8057d326e2741992f68616e415c0c12757
Favour Ori <orifavour2000@gmail.com>
2019-10-18 22:13:45 +0200

    db connection -- Sleepy at this time

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
+app = express();
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+
 //load env
 dotenv.config({ path: "./config/config.env" });
-app = express();
+//Connect to DB
+const connectDB = require("./config/db");
+connectDB();
 
 //Dev logging middleware - only runs when in development
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
@@ -19,6 +23,14 @@ app.use("/api/v1/bootcamps", bootcampsRoutes);
 
 //Spin up server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+//Handle unhandle promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  //close server
+  server.close(() => process.exit(1));
 });
---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDb = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  });
+
+  console.log(`MongoDB connected...${conn.connection.host}`);
+};
+
+module.exports = connectDb;
