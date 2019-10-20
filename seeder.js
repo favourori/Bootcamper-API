9d85c4d536a647f6287f88f9703274b8186df04b
Favour Ori <orifavour2000@gmail.com>
2019-10-20 12:11:50 +0200

    created seeder for bootcamps

---
+++ seeder.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+//load env
+dotenv.config({ path: "./config/config.env" });
+
+//load model
+const Bootcamp = require("./models/Bootcamp");
+
+//connect to DB
+const connectDb = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  });
+
+  console.log(`MongoDB connected...${conn.connection.host}`.yellow.underline);
+};
+
+connectDb();
+//Read Json files
+
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+
+//import into Db
+
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log("Data imported..".green.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+//Delete Data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log("Data deleted..".red.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
+}
